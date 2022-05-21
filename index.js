a9f68725da742017e0f8e77660bdceffbe671fcb
mdsoliman21 <89044891+mdsoliman-me@users.noreply.github.com>
2022-05-21 23:20:32 +0600

    database Conection Success Full

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ app.use(express.json());
 app.use(cors());
 // DB Connection 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3j4fp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect()
+        console.log("Database Connected!")
+    }
+    finally {
 
+    }
+}
+run().catch(console.dir())
 // App Listining Port Veriable 
 app.listen(port, () => {
     console.log(`Doctore App Listining is ${port}`)
